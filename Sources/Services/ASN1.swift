d5ed98e61135fb378f6e5020a46b0d2f925fdb73
Yannick Spreen <yannick.spreen@gmail.com>
2021-05-09 22:13:12 +0200

    Add attribution.

--- Sources/Services/ASN1.swift
+++ Sources/Services/ASN1.swift
@@ -23,6 +23,9 @@
 //
 //  Created by Yannick Spreen on 5/7/21.
 //
+//  Based on ASN1Encoder.swift in ehn-digital-green-development/ValidationCore
+//  by Christian Kollmann
+//
 
 import Foundation
 
