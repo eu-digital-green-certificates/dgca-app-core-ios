ee56eeed480e28a9a28f87124fd4f17119db4064
Yannick Spreen <yannick.spreen@gmail.com>
2021-05-14 12:46:16 +0200

    Change to our json schema fork.

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
     .package(url: "https://github.com/unrelentingtech/SwiftCBOR", from: "0.4.3"),
     .package(url: "https://github.com/SwiftyJSON/SwiftyJSON", from: "5.0.1"),
     .package(url: "https://github.com/Alamofire/Alamofire", from: "5.4.3"),
-    .package(name: "JSONSchema", url: "https://github.com/jnewc/JSONSchema.swift", .branch("master"))
+    .package(name: "JSONSchema", url: "https://github.com/eu-digital-green-certificates/JSONSchema.swift", .branch("master"))
   ],
   targets: [
     // Targets are the basic building blocks of a package. A target can define a module or a test suite.
