2f9ce82dada06cd44147316ac9d8edbcef919690
alexchornyi <85160203+alexchornyi@users.noreply.github.com>
2021-06-09 17:06:58 +0300

    fixed PR from Steffen (#55)
    This pull request should solve the tag type problem and provides validation unit tests/QR codes for testing. Some functions were removed, please evaluate if this was too enthusiastic.
    
    @yspreen comments welcome
    
    Co-authored-by: Alexandr Chernyy <pingus.nikalex@gmail.com>

--- Sources/Services/CBOR.swift
+++ Sources/Services/CBOR.swift
@@ -31,37 +31,97 @@ struct UnwrappedCBOR {
   let payload: SwiftCBOR.CBOR
   let protected: SwiftCBOR.CBOR
   let unprotected: [SwiftCBOR.CBOR: SwiftCBOR.CBOR]
+  let protectedBytes: [UInt8]
+  let payloadBytes: [UInt8]
+  let signatureBytes: [UInt8]
+}
+
+enum CborType: UInt8 {
+    case tag = 210
+    case list = 132
+    case cwt = 216
+    case unknown
+    
+    static func from(data: Data) -> CborType {
+        switch data.first {
+        case self.tag.rawValue: return tag
+        case list.rawValue: return list
+        case cwt.rawValue: return cwt
+        default: return unknown
+        }
+    }
 }
 
 public struct CBOR {
-  static func unwrap(data: Data) -> UnwrappedCBOR? {
-    let decoder = SwiftCBOR.CBORDecoder(input: data.uint)
+    static func unwrap(data: Data) -> UnwrappedCBOR? {
+        
+        let type = CborType.from(data: data);
+        let decoder = SwiftCBOR.CBORDecoder(input: data.uint)
 
-    guard
-      let cbor = try? decoder.decodeItem(),
-      case var SwiftCBOR.CBOR.tagged(tag, cborElement) = cbor
-    else {
-      return nil
-    }
-    if tag.rawValue == cwtTag {
-      guard case let SwiftCBOR.CBOR.tagged(childTag, childElement) = cborElement else {
-        return nil
-      }
-      tag = childTag
-      cborElement = childElement
-    }
-    guard
-      tag.rawValue == coseTag, // SIGN1
-      case let SwiftCBOR.CBOR.array(array) = cborElement,
-      case let SwiftCBOR.CBOR.byteString(protectedBytes) = array[0],
-      let protected = try? SwiftCBOR.CBOR.decode(protectedBytes),
-      case let SwiftCBOR.CBOR.map(unprotectedMap) = array[1],
-      case let SwiftCBOR.CBOR.byteString(payloadBytes) = array[2],
-      let payload = try? SwiftCBOR.CBOR.decode(payloadBytes)
-    else {
-      return nil
-    }
-    return .init(payload: payload, protected: protected, unprotected: unprotectedMap)
+        switch type {
+        case .tag:
+            guard
+              let cbor = try? decoder.decodeItem(),
+              case let SwiftCBOR.CBOR.tagged(_, cborElement) = cbor,
+              case let SwiftCBOR.CBOR.array(array) = cborElement,
+              case let SwiftCBOR.CBOR.byteString(protectedBytes) = array[0],
+              let protected = try? SwiftCBOR.CBOR.decode(protectedBytes),
+              case let SwiftCBOR.CBOR.map(unprotectedMap) = array[1],
+              case let SwiftCBOR.CBOR.byteString(payloadBytes) = array[2],
+              case let SwiftCBOR.CBOR.byteString(signatureBytes) = array[3],
+              let payload = try? SwiftCBOR.CBOR.decode(payloadBytes)
+              else {
+                 return nil
+              }
+            return .init(payload: payload,
+                         protected: protected,
+                         unprotected: unprotectedMap,
+                         protectedBytes: protectedBytes,
+                         payloadBytes: payloadBytes,
+                         signatureBytes: signatureBytes)
+        case .list:
+            guard
+              let cbor=try? SwiftCBOR.CBOR.decode(data.uint),
+              case let SwiftCBOR.CBOR.array(array) = cbor,
+              case let SwiftCBOR.CBOR.byteString(protectedBytes) = array[0],
+              let protected = try? SwiftCBOR.CBOR.decode(protectedBytes),
+              case let SwiftCBOR.CBOR.map(unprotectedMap) = array[1],
+              case let SwiftCBOR.CBOR.byteString(payloadBytes) = array[2],
+              case let SwiftCBOR.CBOR.byteString(signatureBytes) = array[3],
+              let payload = try? SwiftCBOR.CBOR.decode(payloadBytes)
+                else {
+                   return nil
+                }
+            return .init(payload: payload,
+                         protected: protected,
+                         unprotected: unprotectedMap,
+                         protectedBytes: protectedBytes,
+                         payloadBytes: payloadBytes,
+                         signatureBytes: signatureBytes)
+        case .cwt:
+            guard
+             let cbor = try? decoder.decodeItem(),
+             case let SwiftCBOR.CBOR.tagged(_, cborElement) = cbor,
+             case let SwiftCBOR.CBOR.tagged(_, childElement) = cborElement,
+             case let SwiftCBOR.CBOR.array(array) = childElement,
+             case let SwiftCBOR.CBOR.byteString(protectedBytes) = array[0],
+             let protected = try? SwiftCBOR.CBOR.decode(protectedBytes),
+             case let SwiftCBOR.CBOR.map(unprotectedMap) = array[1],
+             case let SwiftCBOR.CBOR.byteString(payloadBytes) = array[2],
+             case let SwiftCBOR.CBOR.byteString(signatureBytes) = array[3],
+             let payload = try? SwiftCBOR.CBOR.decode(payloadBytes)
+             else {
+               return nil
+             }
+            return .init(payload: payload,
+                         protected: protected,
+                         unprotected: unprotectedMap,
+                         protectedBytes: protectedBytes,
+                         payloadBytes: payloadBytes,
+                         signatureBytes: signatureBytes)
+        case .unknown:
+            return nil;
+        }
   }
 
   public static func payload(from data: Data) -> SwiftCBOR.CBOR? {
@@ -93,11 +153,8 @@ public struct CBOR {
   }
 
   public static func hash(from cborData: Data) -> String {
-    let decoder = SwiftCBOR.CBORDecoder(input: cborData.uint)
-
     guard
-      let cbor = try? decoder.decodeItem(),
-      let data = COSE.signedPayloadBytes(from: cbor)
+      let data = COSE.signedPayloadBytes(from: cborData)
     else {
       return ""
     }
