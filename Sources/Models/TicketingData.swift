87e499536c85a286e516fb004c83ce52980d696e
Illia Vlasov <illia.vlasov@trustonic.com>
2021-11-16 18:25:27 +0200

    x5c type changed

--- Sources/Models/TicketingData.swift
+++ Sources/Models/TicketingData.swift
@@ -58,7 +58,7 @@ public struct ServerListResponse : Codable {
 public struct PublicKeyJWK : Codable {
   public var kid : String
   public var alg : String
-  public var x5c : String
+  public var x5c : [String]
   public var use : String
 }
 
