0c21e2d517686f874e46beb8dd1ed8ff5f874336
IgorKhomiak <ihorkhomyak@gmail.com>
2021-11-23 15:35:53 +0200

    Updated Core lib name

---
+++ CoreComponent/CoreProject/coreProject.h
@@ -0,0 +1,18 @@
+//
+//  coreProject.h
+//  CoreProject
+//
+//  Created by Yannick Spreen on 5/8/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CoreProject.
+FOUNDATION_EXPORT double coreProjectVersionNumber;
+
+//! Project version string for CoreProject.
+FOUNDATION_EXPORT const unsigned char coreProjectVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <coreProject/PublicHeader.h>
+
+
