0173e3194fee7e5611cba2a9391e64e189db3e07
IgorKhomiak <ihorkhomyak@gmail.com>
2022-01-19 22:44:05 +0200

    minor

--- Sources/Models/CoreManager.swift
+++ Sources/Models/CoreManager.swift
@@ -32,14 +32,12 @@ import AppKit
 
 public class CoreManager {
     public static var shared = CoreManager()
-    
+    lazy public var config = HCertConfig.default
+    public static var publicKeyEncoder: PublicKeyStorageDelegate?
+
 #if os(iOS)
     public static var cachedQrCodes = SyncDict<UIImage>()
 #else
     public static var cachedQrCodes = SyncDict<NSImage>()
 #endif
-
-    public static var publicKeyEncoder: PublicKeyStorageDelegate?
-
-    lazy public var config = HCertConfig.default
 }
